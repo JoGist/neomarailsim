52b7f999aa0ab3f77354c6ba1abf4a421bdc7913
Giovanni Roma <r.giovanni.98@gmail.com>
2020-09-21 22:17:32 +0200

    Added downloads

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -16,6 +16,29 @@ buttonsArray.forEach(function (el){
 });
 
 
+$(document).ready(function(){
+  var price = $(".price").text().trim();
+  var quantity = $("#quantity").val();
+  console.log(quantity);
+  //add listener to plus button
+  $("#plus").click(function(e){
+    e.preventDefault();
+    quantity++;
+    $("#quantity").val(quantity);
+    $(".price").text(quantity*price);
+  });
+  $("#minus").click(function(e){
+    e.preventDefault();
+    if(quantity>1){
+      quantity--;
+      $("#quantity").val(quantity);
+      $(".price").text(quantity*price);
+    }
+  });
+  //set total price
+  
+});
+
 
 /*$('.slider').each(function() {
     var $this = $(this);
